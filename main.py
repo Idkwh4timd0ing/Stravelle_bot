f7eacf17ba4a06a4d6536b0d0c600bbe9aa7b14e
Lia <idkwhat.imdoing@outlook.fr>
2025-05-04 14:08:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
 import os
+from supabase import create_client, Client
+import os
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents)
